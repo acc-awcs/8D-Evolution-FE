118fa2eab6bbe92782eb1ea87539fc91e4c01ca9
Emi Jackson <emij@umich.edu>
2025-12-30 19:59:48 -0500

    fix/start quiz not prerenderable

--- src/routes/poll/[pollCode]/quiz/+page.ts
+++ src/routes/poll/[pollCode]/quiz/+page.ts
@@ -7,10 +7,3 @@ export function load({ params }) {
 		dynamics
 	};
 }
-
-// we need JS for scroll behavior and form submission
-export const csr = true;
-
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
